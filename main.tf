2badf3a0d7ae9a76a09ab6e3b55a3462bdd40ff1
chirap123 <109028298+chirap123@users.noreply.github.com>
2022-08-30 18:10:55 +0530

    Add files via upload
    rds_instance

--- main.tf
+++ main.tf
@@ -1,16 +1,36 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
     }
   }
+
+  required_version = ">= 0.14.9"
 }
 
-resource "aws_instance" "todo" {
-  ami           = var.ami
-  instance_type = var.instance_type
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "AKIAVHEAXJ32GN746Z5I"
+  secret_key = "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"
+}
 
-  credit_specification {
-    cpu_credits = "unlimited"
+
+resource "aws_db_instance" "todo_rds_instance" {
+  allocated_storage   = 20
+  identifier          = "rds-terraform"
+  storage_type        = "gp2"
+  engine              = "mysql"
+  engine_version      = "8.0.27"
+  instance_class      = "db.t2.micro"
+  name                = "todo_db"
+  username            = "todo"
+  password            = "todo12345"
+  publicly_accessible = true
+  skip_final_snapshot = true
+
+
+  tags = {
+    Name = "ExampleRDSServerInstance"
   }
 }
